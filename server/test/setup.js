2c08b18e0e16ff8598cc67c316a8637cb02d73a5
Brent Gardner <bgardner@squarelabs.net>
2018-07-31 11:48:14 +0530

    async setup

--- server/test/setup.js
+++ server/test/setup.js
@@ -1,11 +1,20 @@
-console.log('------ setting up tests ---------');
-process.env.VCAP_SERVICES = {
-    mysql: {
-        credentials: {
-            host: '127.0.0.1',
-            user: 'root',
-            password: 'password',
-            name: 'modern_test'
+before(() => {
+    console.log('------ setting up tests ---------');
+    process.env.VCAP_SERVICES = {
+        mysql: {
+            credentials: {
+                host: '127.0.0.1',
+                user: 'root',
+                password: 'password',
+                name: 'modern_test'
+            }
         }
     }
-}
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('------ running tests ---------');
+            resolve();
+        }, 1000);
+    });
+});
+
