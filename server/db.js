3002cb70219a308cabc0d704eb0740f2c675bb9d
Brent Gardner <bgardner@squarelabs.net>
2018-07-31 11:27:23 +0530

    vcap

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 const connect = require('knex');
+const vcap = process.env.VCAP_SERVICES ? JSON.parse(process.env.VCAP_SERVICES) : undefined;
 const config = {
     client: 'mysql',
     connection: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'password',
-        database: 'modern'
+        host: vcap ? vcap.mysql.credentials.host : '127.0.0.1',
+        user: vcap ? vcap.mysql.credentials.user : 'root',
+        password: vcap ? vcap.mysql.credentials.password : 'password',
+        database: vcap ? vcap.mysql.credentials.name : 'modern'
     }
 };
 const pool = connect(config);
