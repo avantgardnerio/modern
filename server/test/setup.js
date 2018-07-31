31810522d425851d853e99c31ab9e79e2732ec88
Brent Gardner <bgardner@squarelabs.net>
2018-07-31 12:02:07 +0530

    running tests

--- server/test/setup.js
+++ server/test/setup.js
@@ -1,20 +1,23 @@
-before(() => {
-    console.log('------ setting up tests ---------');
-    process.env.VCAP_SERVICES = {
-        mysql: {
-            credentials: {
-                host: '127.0.0.1',
-                user: 'root',
-                password: 'password',
-                name: 'modern_test'
+before(async () => {
+    try {
+        console.log('------ setting up tests ---------');
+        process.env.VCAP_SERVICES = JSON.stringify({
+            mysql: {
+                credentials: {
+                    host: '127.0.0.1',
+                    user: 'root',
+                    password: 'password',
+                    name: 'modern_test'
+                }
             }
-        }
+        });
+        const knex = require('../db');
+        await knex.migrate.latest();
+        await knex.destroy();
+        console.log('------ running tests ---------');
+    } catch(ex) {
+        console.error(ex);
+        process.exit(-1);
     }
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('------ running tests ---------');
-            resolve();
-        }, 1000);
-    });
 });
 
