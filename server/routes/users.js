cbf89fb658d02f9f53f8fd5f3e038cc02e9faf10
Brent Gardner <bgardner@squarelabs.net>
2018-07-31 12:24:16 +0530

    can return users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db');
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  const users = await knex.select().table('person');
+  res.json(users);
 });
 
 module.exports = router;
