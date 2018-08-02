174b5eefb6b0d1ad2045f95b0e11469637d5a890
Brent Gardner <bgardner@squarelabs.net>
2018-08-02 11:14:35 +0530

    oracle

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-docker run -v `pwd`:/workspace -p 3000:3000 -it modern /bin/bash -c "service mysql start && bash"
+docker run \
+    -v `pwd`:/workspace \
+    -p 3000:3000 \
+    -p 1521:1521 \
+    -it modern /bin/bash -c "/home/oracle/setup/dockerInit.sh & bash"
